4f16963464e26576f7bb905c9bbe2d594ce7ba8f
rikkyps <rikky.putra14@gmail.com>
2024-01-13 06:30:37 +0700

    add: modul register

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { render } from 'react-dom'
-import { createInertiaApp } from "@inertiajs/inertia-react";
-import { InertiaProgress } from "@inertiajs/progress";
-import { resolvePageCommponent } from 'laravel-vite-plugin'
+import React from 'react';
+import { render } from 'react-dom';
+import { createInertiaApp } from '@inertiajs/inertia-react';
+import { InertiaProgress } from '@inertiajs/progress';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-    resolve: (name) => resolvePageCommponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({el, App, props}) {
-        return render(<App {...props} />, el)
-    }
-})
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    setup({ el, App, props }) {
+        return render(<App {...props} />, el);
+    },
+});
 
 InertiaProgress.init({
     delay: 1,
-    color: '#ffffff'
-})
+    color: '#8F4030',
+});
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//AUTH
+Route::get('/login', [App\Http\Controllers\Auth\RegisterController::class, 'index'])->name('register')->middleware('guest');
+Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'index'])->name('register')->middleware('guest');
+Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'store'])->name('register.store')->middleware('guest');
